2e11f04c8772e04e81dd49cf22b7d1fb37476fb3
Nate Lager <nlager@redhat.com>
2025-07-03 13:16:09 -0400

    fixing some deps, and cleaning up setu-rhel.sh

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Create an output file
-touch /root/post-run.log
+#touch /root/post-run.log
 # configure rhel account
-usermod -aG wheel rhel
-echo redhat | passwd --stdin rhel
+#usermod -aG wheel rhel
+#echo redhat | passwd --stdin rhel
 
-subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force
+#subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force
 
-dnf -y install sssd-proxy
+#dnf -y install sssd-proxy
 
 # setup webui
 # Create a done file to signal we have finished
-touch /root/post-run.log.done
+#touch /root/post-run.log.done
